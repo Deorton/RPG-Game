eb3e9d019bbc29b15596d0c95c9be96c93554597
Deorton <steve.raybone@hotmail.co.uk>
2025-09-07 00:00:10 +0100

    Fading in And Out of scenes
    fading system

---
+++ Assets/RPG Game Files/Scripts/Core/PersistantObjectSpawner.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistantObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject persistantObjectPrefab;
+        static bool hasSpawned = false;
+
+        void Awake()
+        {
+            if (hasSpawned) return;
+            
+            SpawnPersistantObject();
+
+            hasSpawned = true;
+        }
+
+        private void SpawnPersistantObject()
+        {
+            GameObject persistantObject = Instantiate(persistantObjectPrefab);
+            DontDestroyOnLoad(persistantObject);
+        }
+    }
+}
