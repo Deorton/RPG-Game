aa7261eb589184c9aa9cc18f39d0253884957382
Deorton <steve.raybone@hotmail.co.uk>
2025-09-21 19:08:36 +0100

    start of damage text
    floating damage numbers

---
+++ Assets/RPG Game Files/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
